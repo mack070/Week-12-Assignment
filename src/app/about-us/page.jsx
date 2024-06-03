985b0524ee77710996d7d054f4a83693ae2f142b
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-06-03 15:51:09 +0100

    Update page.jsx

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,28 @@
+export default function NewYork() {
+    return (
+        <div>
+<header className="about-us-logo">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dd139449b300bd8e145eb/previews/665dd139449b300bd8e145f5/download/My_first_design_1_(6).jpeg" alt="About Us Logo"></img>
+</header>
+<div>
+<h1>Add information here about us</h1>
+</div>
+<div>
+<h2>Mel</h2>
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dd139449b300bd8e145eb/previews/665dd139449b300bd8e145f5/download/My_first_design_1_(6).jpeg" alt="Mel"></img>
+</div>
+<div>
+<h2>Jerry</h2>
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dd139449b300bd8e145eb/previews/665dd139449b300bd8e145f5/download/My_first_design_1_(6).jpeg" alt="Jerry"></img>
+</div>
+<div>
+<h2>Toyah</h2>
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dd139449b300bd8e145eb/previews/665dd139449b300bd8e145f5/download/My_first_design_1_(6).jpeg" alt="Toyah"></img>
+</div>
+<div>
+<h2>Emma</h2>
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dd139449b300bd8e145eb/previews/665dd139449b300bd8e145f5/download/My_first_design_1_(6).jpeg" alt="Emma"></img>
+</div>
+</div>
+    );
+}
