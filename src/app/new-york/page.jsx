f91fedf44054b1fd2e512a9e11dc8b2e0cc53aad
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-06-03 15:12:48 +0100

    template for holiday pages

--- src/app/new-york/page.jsx
+++ src/app/new-york/page.jsx
@@ -1,7 +1,29 @@
 export default function NewYork() {
     return (
-<div className="new-york-logo">
+        <div>
+<header className="new-york-logo">
 <img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="New York Logo"></img>
+</header>
+<div className="ny-image-one">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image one"></img>
+<div className="ny-image-two">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image two"></img>
+</div>
+<div className="ny-image-three">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image three"></img>
+</div>
+</div>
+<h1>New York Holiday Information</h1>
+<h2>Add information here about New York...</h2>
+<div className="ny-image-three">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image three"></img>
+</div>
+<div className="ny-image-three">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image three"></img>
+</div>
+<div className="ny-image-three">
+<img src="https://trello.com/1/cards/665dc790014f1f79fe9d1fd1/attachments/665dc7a94e55738e29d2da54/previews/665dc7aa4e55738e29d2db87/download/My_first_design_1_(2).jpeg" alt="ny image three"></img>
+</div>
 </div>
     );
 }
